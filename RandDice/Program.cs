b3fa8c2c05afd5a3c0a652a3f8ba13e3b63ce133
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-26 15:39:01 +0000

    working randdice

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,23 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            string numeroDados = args[0];
+            string semente = args[1];
+
+            int numeroDadosInt = int.Parse(numeroDados);
+            int sementeInt = int.Parse(semente);
+
+            Random dado = new Random(sementeInt);
+
+            int valorTotal = 0;
+
+            for(int i = 0; i < numeroDadosInt; i++)
+            {
+                int valor = dado.Next(1,7);
+                valorTotal += valor;
+            }
+            
+            Console.WriteLine(valorTotal);
         }
     }
 }
